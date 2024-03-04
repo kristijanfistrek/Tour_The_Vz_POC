f6f7f06c56c85cb9be6d2fafc2d12b6e7ff4f2b2
kristijanfistrek <fistrekkristijan@gmail.com>
2024-03-04 14:36:28 +0100

    Add: initial commit

---
+++ src/main/java/codeit/lab/tourthevz/TourthevzApplication.java
@@ -0,0 +1,13 @@
+package codeit.lab.tourthevz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourthevzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourthevzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeit/lab/tourthevz/TourthevzApplicationTests.java
@@ -0,0 +1,13 @@
+package codeit.lab.tourthevz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourthevzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
